b4b45b3bcdf47cc3657ed4be9d201dd51290c231
Robert Buj <robert.buj@gmail.com>
2026-07-17 15:13:14 +0200

    npx npm-check-updates -u

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ const distPath = path.join(__dirname, '../../dist');
 
 app.use(express.static(distPath));
 
-app.get('*', (_req, res) => {
+app.get('/{*path}', (_req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
